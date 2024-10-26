874579c9504df11fac6259d8b385be76249320f8
Jacotet <cuentafree2003@gmail.com>
2024-10-26 19:08:09 +0200

    Mazo - primeras ideas
    Agregado unas cuantas funciones y constructores a la clase Mazo, más dos tests importantes

---
+++ src/main/java/main/Model/Mazo.java
@@ -0,0 +1,26 @@
+package main.Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class Mazo{
+    private List<Carta> mazo;
+    private Stack<Carta> descarte;
+
+    public Mazo() {} //constructor
+    
+    public void barajar() {} 
+    public Carta robarCarta() {}
+    private void renovarMazo() {}
+
+    public List<Carta> getMazoCartas() {
+        return mazo.size();
+    }
+
+
+
+
+
+}
